9ca6373763255d2e6287c6bed08bf9bd7d4f8cb4
Dmitriy <165094851+wa-tl-ok@users.noreply.github.com>
2024-06-13 22:29:57 +0300

    Create Informatics task 745  Hashing with removal.cpp

---
+++ Other/Hash/Informatics task 745  Hashing with removal.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <array>
+#include <list>
+#include <stack>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <string>
+#include <cstring>
+#include <random>
+#include <bitset>
+#include <functional>
+#include <random>
+
+using namespace std;
+
+using ll = long long;
+using str = string;
+
+ll mod = 1e9 + 7;
+char type; str s;
+vector<ll> step(11);
+map<ll, bool> m;
+
+int main() {
+    type = '!';
+    step[0] = 1;
+    for (int i = 1; i < 11; ++i) {
+        step[i] = step[i - 1] * 31;
+        step[i] %= mod;
+    }
+    while (true) {
+        cin >> type;
+        if (type == '#') {
+            break;
+        }
+        cin >> s;
+        ll Hash = 0;
+        for (int i = 0; i < (int)s.size(); ++i) {
+            Hash += step[i] * (s[i] - 'a' + 1);
+            Hash %= mod;
+        }
+        if (type == '-') {
+            m[Hash] = false;
+        }
+        else if (type == '+') {
+            m[Hash] = true;
+        }
+        else {
+            if (m[Hash] == true) {
+                cout << "YES\n";
+            }
+            else {
+                cout << "NO\n";
+            }
+        }
+    }
+}
