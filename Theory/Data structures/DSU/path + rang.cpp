4060aeb055156236f5d10c01fd6dbf2dcde6ac93
Dmitriy <165094851+wa-tl-ok@users.noreply.github.com>
2024-07-22 21:39:50 +0300

    Update path + rang.cpp

--- Theory/Data structures/DSU/path + rang.cpp
+++ Theory/Data structures/DSU/path + rang.cpp
@@ -1,24 +1,3 @@
-#include <iostream>
-#include <algorithm>
-#include <cmath>
-#include <vector>
-#include <queue>
-#include <deque>
-#include <array>
-#include <list>
-#include <stack>
-#include <set>
-#include <unordered_set>
-#include <map>
-#include <unordered_map>
-#include <string>
-#include <cstring>
-#include <random>
-#include <bitset>
-#include <functional>
-
-using namespace std;
-
 struct DSU {
     vector<int> head;
     vector<int> rang;
@@ -63,11 +42,3 @@ struct DSU {
         return true;
     }
 };
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-
-    return 0;
-}
