a4d32b9d048bdac9b4b3ed92f835b75bf80ff2b0
Dmitriy <165094851+wa-tl-ok@users.noreply.github.com>
2024-07-22 21:41:40 +0300

    Create weight.cpp

---
+++ Theory/Data structures/DSU/weight.cpp
@@ -0,0 +1,41 @@
+struct DSU {
+    vector<int> head;
+    vector<int> rang;
+ 
+    DSU(int n) {
+        head.resize(n);
+        for (int i = 0; i < n; i++) {
+            head[i] = i;
+        }
+ 
+        rang.resize(n);
+        for (int i = 0; i < n; i++) {
+            rang[i] = 1;
+        }
+    }
+ 
+    int Find(int x) {
+        if (head[x] == x) {
+            return x;
+        }
+        return Find(head[x]);
+    }
+ 
+    bool Union(int x, int y) {
+        x = Find(x);
+        y = Find(y);
+ 
+        if (x == y) {
+            return false;
+        }
+ 
+        if (rang[x] > rang[y]) {
+            swap(x, y);
+        }
+ 
+        rang[y] += rang[x];
+        head[x] = y;
+ 
+        return true;
+    }
+};
