2ab0afa892d978a1b90b8d448b0f45988c460ac5
Dmitriy <165094851+wa-tl-ok@users.noreply.github.com>
2024-07-25 20:00:55 +0300

    Update Segment Tree (Min).cpp

--- Theory/Data structures/Segment Tree/Segment Tree (Min).cpp
+++ Theory/Data structures/Segment Tree/Segment Tree (Min).cpp
@@ -1,16 +1,31 @@
-class SegmentTreeMin {
+class SegmentTree {
 public:
-    SegmentTreeMin(int size) {
+    SegmentTree(int size) {
         n = size;
-        tree.assign(4 * n, INT_MAX);
-        lazy.assign(4 * n, 0);
+        int p = 1;
+        while (p < n) {
+            p *= 2;
+        }
+
+        tree.assign(p * 2, 2e9);
+        lazy.assign(p * 2, { 0, false });
+
+        n = p;
     }
 
-    SegmentTreeMin(const vector<int>& a) {
+    SegmentTree(const vector<int>& a) {
         n = (int)a.size();
-        tree.assign(4 * n, INT_MAX);
-        lazy.assign(4 * n, 0);
-        for (int i = 0; i < n; ++i) {
+        int p = 1;
+        while (p < n) {
+            p *= 2;
+        }
+
+        tree.assign(p * 2, 2e9);
+        lazy.assign(p * 2, { 0, false });
+
+        n = p;
+
+        for (int i = 0; i < (int)a.size(); ++i) {
             updateRange(1, 0, n - 1, i, i, a[i], false);
         }
     }
@@ -23,24 +38,36 @@ public:
         updateRange(1, 0, n - 1, const_l, const_r, val, false);
     }
 
-    int queryMin(int const_l, int const_r) {
-        return queryMinRange(1, 0, n - 1, const_l, const_r);
+    int query(int const_l, int const_r) {
+        return queryRange(1, 0, n - 1, const_l, const_r);
     }
-
 private:
     vector<int> tree;
-    vector<int> lazy;
+    vector<pair<int, bool>> lazy;
     int n;
 
     void propagate(int node, int l, int r) {
-        if (lazy[node] != 0) {
-            tree[node] += lazy[node];
-            if (l != r) {
-                lazy[2 * node] += lazy[node];
-                lazy[2 * node + 1] += lazy[node];
+        if (lazy[node].second) {
+            if (lazy[node].first != 0) {
+                tree[node] += lazy[node].first;
+                if (l != r) {
+                    lazy[2 * node].first += lazy[node].first;
+                    lazy[2 * node].second = true;
+                    lazy[2 * node + 1].first += lazy[node].first;
+                    lazy[2 * node + 1].second = true;
+                }
             }
-            lazy[node] = 0;
         }
+        else {
+            if (lazy[node].first != 0) {
+                tree[node] = lazy[node].first;
+                if (l != r) {
+                    lazy[2 * node] = lazy[node];
+                    lazy[2 * node + 1] = lazy[node];
+                }
+            }
+        }
+        lazy[node] = { 0, false };
     }
 
     void updateRange(int node, int l, int r, int const_l, int const_r, int val, bool isAddition) {
@@ -52,35 +79,46 @@ private:
             if (isAddition) {
                 tree[node] += val;
                 if (l != r) {
-                    lazy[2 * node] += val;
-                    lazy[2 * node + 1] += val;
+                    lazy[2 * node].first += val;
+                    lazy[2 * node].second = true;
+                    lazy[2 * node + 1].first += val;
+                    lazy[2 * node + 1].second = true;
                 }
-            } else {
+            }
+            else {
                 tree[node] = val;
                 if (l != r) {
-                    lazy[2 * node] = val;
-                    lazy[2 * node + 1] = val;
+                    lazy[2 * node] = { val, false };
+                    lazy[2 * node + 1] = { val, false };
                 }
             }
             return;
         }
+
         int mid = (l + r) / 2;
+
         updateRange(2 * node, l, mid, const_l, const_r, val, isAddition);
         updateRange(2 * node + 1, mid + 1, r, const_l, const_r, val, isAddition);
+
         tree[node] = min(tree[2 * node], tree[2 * node + 1]);
     }
 
-    int queryMinRange(int node, int l, int r, int const_l, int const_r) {
+    int queryRange(int node, int l, int r, int const_l, int const_r) {
         if (l > r || l > const_r || r < const_l) {
-            return INT_MAX;
+            return 2e9;
         }
+
         propagate(node, l, r);
+
         if (l >= const_l && r <= const_r) {
             return tree[node];
         }
+
         int mid = (l + r) / 2;
-        int p1 = queryMinRange(2 * node, l, mid, const_l, const_r);
-        int p2 = queryMinRange(2 * node + 1, mid + 1, r, const_l, const_r);
+
+        int p1 = queryRange(2 * node, l, mid, const_l, const_r);
+        int p2 = queryRange(2 * node + 1, mid + 1, r, const_l, const_r);
+
         return min(p1, p2);
     }
 };
