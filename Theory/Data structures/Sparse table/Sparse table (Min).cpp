5658c71c37e145733846a9da05ce5ceff5071afe
Dmitriy <165094851+wa-tl-ok@users.noreply.github.com>
2024-08-03 00:55:52 +0300

    Update Sparse table (Min).cpp

--- Theory/Data structures/Sparse table/Sparse table (Min).cpp
+++ Theory/Data structures/Sparse table/Sparse table (Min).cpp
@@ -4,31 +4,14 @@ private:
     vector<vector<int>> Stable;
 public:
     Sparse_table_min(const vector<int>& vec) {
-        vector<int> new_vec;
-        for (auto p : vec) {
-            new_vec.push_back(p);
-        }
+        this->N = (int)(vec.size());
+        this->K = log2(N) + 2;
 
-        this->K = 0;
-
-        int n = (int)vec.size();
-        int p = 1;
-        while (p < n) {
-            p *= 2;
-            ++(this->K);
-        }
-
-        for (int i = 0; i < p - n; i++) {
-            new_vec.push_back(INT_MAX);
-        }
-
-        this->N = (int)new_vec.size();
-
-        Stable.resize(N);
+        Stable.resize(N + 1);
 
         for (int i = 0; i < N; i++) {
-            Stable[i].resize(K);
-            Stable[i][0] = new_vec[i];
+            Stable[i].resize(K + 1);
+            Stable[i][0] = vec[i];
         }
 
         for (int j = 1; (1 << j) <= N; j++) {
@@ -39,14 +22,15 @@ public:
     }
 
     int query(int l, int r) {
-        int mn = INT_MAX;
+        int mn = 1e9;
         int sz = r - l + 1;
 
         for (int k = K; k >= 0; k--) {
             if (sz >= (1 << k)) {
                 mn = min(mn, Stable[l][k]);
-                sz -= (1 << k);
-                l += (1 << k);
+                sz -= 1 << k;
+
+                l += 1 << k;
             }
         }
 
