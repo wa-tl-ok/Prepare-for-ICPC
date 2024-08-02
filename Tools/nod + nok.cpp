aab7db0dc571bc7bff836bd48d0795c0cb9bef1d
Dmitriy <165094851+wa-tl-ok@users.noreply.github.com>
2024-08-03 00:14:46 +0300

    Update nod + nok.cpp

--- Tools/nod + nok.cpp
+++ Tools/nod + nok.cpp
@@ -1,25 +1,3 @@
-#include <iostream>
-#include <algorithm>
-#include <cmath>
-#include <vector>
-#include <queue>
-#include <deque>
-#include <array>
-#include <list>
-#include <stack>
-#include <set>
-#include <unordered_set>
-#include <map>
-#include <unordered_map>
-#include <string>
-#include <cstring>
-#include <random>
-#include <bitset>
-#include <functional>
-#include <random>
-
-using namespace std;
-
 int gcd(int a, int b) {
     while (b != 0) {
         int temp = b; 
@@ -35,12 +13,4 @@ int mathnod(int a, int b) {
 
 int mathnok(int a, int b) { 
     return a * b / mathnod(a, b); 
-}
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-
-    return 0;
 }
