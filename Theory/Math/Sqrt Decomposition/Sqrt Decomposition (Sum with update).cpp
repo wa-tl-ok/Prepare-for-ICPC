422931540fd98afa36bf743431984fb2eb898fdc
Dmitriy <165094851+wa-tl-ok@users.noreply.github.com>
2024-08-05 00:52:38 +0300

    Update Sqrt Decomposition (Sum with update).cpp

--- Theory/Math/Sqrt Decomposition/Sqrt Decomposition (Sum with update).cpp
+++ Theory/Math/Sqrt Decomposition/Sqrt Decomposition (Sum with update).cpp
@@ -4,7 +4,7 @@ public:
         n = a.size();
         block_size = sqrt(n) + 1;
         blocks.resize(block_size, 0);
-        lazy.resize(block_size, 0);
+        lazy.resize(block_size, { 0, 0 });
         vec = a;
 
         for (int i = 0; i < n; ++i) {
@@ -33,7 +33,12 @@ public:
             }
 
             for (int block = start_block + 1; block < end_block; ++block) {
-                sum += blocks[block] + lazy[block] * block_size;
+                if (lazy[block].second) {
+                    sum += lazy[block].first * block_size;
+                }
+                else {
+                    sum += blocks[block] + lazy[block].first * block_size;
+                }
             }
 
             apply_lazy(end_block);
@@ -45,7 +50,7 @@ public:
         return sum;
     }
 
-    void update(int l, int r, int value) {
+    void plus(int l, int r, int value) {
         int start_block = l / block_size;
         int end_block = r / block_size;
 
@@ -67,7 +72,7 @@ public:
             }
 
             for (int block = start_block + 1; block < end_block; ++block) {
-                lazy[block] += value;
+                lazy[block].first += value;
             }
 
             apply_lazy(end_block);
@@ -78,20 +83,67 @@ public:
         }
     }
 
+    void change(int l, int r, int value) {
+        int start_block = l / block_size;
+        int end_block = r / block_size;
+
+        if (start_block == end_block) {
+            apply_lazy(start_block);
+            for (int i = l; i <= r; ++i) {
+                vec[i] = value;
+            }
+            rebuild_block(start_block);
+        }
+        else {
+            int end_L = (start_block + 1) * block_size - 1;
+            int start_R = end_block * block_size;
+
+            apply_lazy(start_block);
+            for (int i = l; i <= end_L; ++i) {
+                vec[i] = value;
+            }
+            rebuild_block(start_block);
+
+            for (int block = start_block + 1; block < end_block; ++block) {
+                lazy[block] = { value, 1 };
+            }
+
+            apply_lazy(end_block);
+            for (int i = start_R; i <= r; ++i) {
+                vec[i] = value;
+            }
+            rebuild_block(end_block);
+        }
+    }
+
 private:
     int n;
     int block_size;
     vector<int> blocks;
-    vector<int> lazy;
+    vector<pair<int, int>> lazy;
     vector<int> vec;
 
     void apply_lazy(int block) {
-        if (lazy[block] != 0) {
+        if (lazy[block].second) {
             for (int i = block * block_size; i < (block + 1) * block_size && i < n; ++i) {
-                vec[i] += lazy[block];
+                vec[i] = lazy[block].first;
             }
-            blocks[block] += lazy[block] * block_size;
-            lazy[block] = 0;
+            blocks[block] = lazy[block].first * block_size;
+            lazy[block] = { 0, 0 };
+        }
+        else if (lazy[block].first != 0) {
+            for (int i = block * block_size; i < (block + 1) * block_size && i < n; ++i) {
+                vec[i] += lazy[block].first;
+            }
+            blocks[block] += lazy[block].first * block_size;
+            lazy[block].first = 0;
+        }
+    }
+
+    void rebuild_block(int block) {
+        blocks[block] = 0;
+        for (int i = block * block_size; i < (block + 1) * block_size && i < n; ++i) {
+            blocks[block] += vec[i];
         }
     }
 };
