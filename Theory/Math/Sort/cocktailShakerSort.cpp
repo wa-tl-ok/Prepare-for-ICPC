663c4a099ebb99ac2ba8a8ffecda40ea30bea65d
Dmitriy <165094851+wa-tl-ok@users.noreply.github.com>
2024-08-07 13:36:16 +0300

    Create cocktailShakerSort.cpp

---
+++ Theory/Math/Sort/cocktailShakerSort.cpp
@@ -0,0 +1,30 @@
+void cocktailShakerSort(vector<int>& arr) {
+    bool swapped = true;
+    int start = 0;
+    int end = arr.size() - 1;
+
+    while (swapped) {
+        swapped = false;
+
+        for (int i = start; i < end; ++i) {
+            if (arr[i] > arr[i + 1]) {
+                swap(arr[i], arr[i + 1]);
+                swapped = true;
+            }
+        }
+
+        if (!swapped) break;
+
+        swapped = false;
+        --end;
+
+        for (int i = end - 1; i >= start; --i) {
+            if (arr[i] > arr[i + 1]) {
+                swap(arr[i], arr[i + 1]);
+                swapped = true;
+            }
+        }
+
+        ++start;
+    }
+}
