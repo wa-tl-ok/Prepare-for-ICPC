c7f8667180a0eb38b36e3725e4114e2a040e77c5
Dmitriy <165094851+wa-tl-ok@users.noreply.github.com>
2024-08-07 10:29:48 +0300

    Create Task 4.cpp

---
+++ Theory/Math/DP/Theme 1/Task 4.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <array>
+#include <list>
+#include <stack>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <string>
+#include <cstring>
+#include <random>
+#include <bitset>
+#include <functional>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n; cin >> n;
+
+    vector<int> a(1005);
+    a[0] = 1;
+    a[1] = 1;
+
+    for (int i = 2; i < 1005; i++) {
+        if (i % 2 == 0) {
+            a[i] = a[i / 2] + a[i / 2 - 1];
+        }
+        else {
+            a[i] = a[i / 2] - a[i / 2 - 1];
+        }
+    }
+
+    cout << a[n];
+
+    return 0;
+}
