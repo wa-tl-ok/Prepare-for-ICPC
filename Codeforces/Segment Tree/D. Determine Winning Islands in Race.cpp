0975715bdb0b34a82e472d0af193000e7de71d89
Dmitriy <165094851+wa-tl-ok@users.noreply.github.com>
2024-08-10 23:35:03 +0300

    Create D. Determine Winning Islands in Race.cpp

---
+++ Codeforces/Segment Tree/D. Determine Winning Islands in Race.cpp
@@ -0,0 +1,211 @@
+//https://codeforces.com/contest/1998/problem/D?locale=en
+
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <array>
+#include <list>
+#include <stack>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <string>
+#include <cstring>
+#include <random>
+#include <bitset>
+#include <functional>
+
+using namespace std;
+
+class SegmentTree {
+public:
+    SegmentTree(int size) {
+        n = size;
+        int p = 1;
+        while (p < n) {
+            p *= 2;
+        }
+
+        tree.assign(p * 2, 0);
+        lazy.assign(p * 2, { 0, false });
+
+        n = p;
+    }
+
+    SegmentTree(const vector<int>& a) {
+        n = (int)a.size();
+        int p = 1;
+        while (p < n) {
+            p *= 2;
+        }
+
+        tree.assign(p * 2, 0);
+        lazy.assign(p * 2, { 0, false });
+
+        n = p;
+
+        for (int i = 0; i < (int)a.size(); ++i) {
+            updateRange(1, 0, n - 1, i, i, a[i], false);
+        }
+    }
+
+    void plus(int const_l, int const_r, int val) {
+        updateRange(1, 0, n - 1, const_l, const_r, val, true);
+    }
+
+    void change(int const_l, int const_r, int val) {
+        updateRange(1, 0, n - 1, const_l, const_r, val, false);
+    }
+
+    int query(int const_l, int const_r) {
+        return queryRange(1, 0, n - 1, const_l, const_r);
+    }
+private:
+    vector<int> tree;
+    vector<pair<int, bool>> lazy;
+    int n;
+
+    void propagate(int node, int l, int r) {
+        if (lazy[node].second) {
+            if (lazy[node].first != 0) {
+                tree[node] += lazy[node].first * (r - l + 1);
+                if (l != r) {
+                    lazy[2 * node].first += lazy[node].first;
+                    lazy[2 * node].second = true;
+                    lazy[2 * node + 1].first += lazy[node].first;
+                    lazy[2 * node + 1].second = true;
+                }
+            }
+        }
+        else {
+            if (lazy[node].first != 0) {
+                tree[node] = lazy[node].first * (r - l + 1);
+                if (l != r) {
+                    lazy[2 * node] = lazy[node];
+                    lazy[2 * node + 1] = lazy[node];
+                }
+            }
+        }
+        lazy[node] = { 0, false };
+    }
+
+    void updateRange(int node, int l, int r, int const_l, int const_r, int val, bool isAddition) {
+        propagate(node, l, r);
+        if (l > r || l > const_r || r < const_l) {
+            return;
+        }
+        if (l >= const_l && r <= const_r) {
+            if (isAddition) {
+                tree[node] += val * (r - l + 1);
+                if (l != r) {
+                    lazy[2 * node].first += val;
+                    lazy[2 * node].second = true;
+                    lazy[2 * node + 1].first += val;
+                    lazy[2 * node + 1].second = true;
+                }
+            }
+            else {
+                tree[node] = val * (r - l + 1);
+                if (l != r) {
+                    lazy[2 * node] = { val, false };
+                    lazy[2 * node + 1] = { val, false };
+                }
+            }
+            return;
+        }
+
+        int mid = (l + r) / 2;
+
+        updateRange(2 * node, l, mid, const_l, const_r, val, isAddition);
+        updateRange(2 * node + 1, mid + 1, r, const_l, const_r, val, isAddition);
+
+        tree[node] = tree[2 * node] + tree[2 * node + 1];
+    }
+
+    int queryRange(int node, int l, int r, int const_l, int const_r) {
+        if (l > r || l > const_r || r < const_l) {
+            return 0;
+        }
+
+        propagate(node, l, r);
+
+        if (l >= const_l && r <= const_r) {
+            return tree[node];
+        }
+
+        int mid = (l + r) / 2;
+
+        int p1 = queryRange(2 * node, l, mid, const_l, const_r);
+        int p2 = queryRange(2 * node + 1, mid + 1, r, const_l, const_r);
+
+        return p1 + p2;
+    }
+};
+
+void Solve() {
+    int n, m; cin >> n >> m;
+
+    vector<int> U(m);
+    vector<int> V(m);
+
+    vector<vector<int>> g(n + 1);
+    for (int i = 0; i < m; i++) {
+        int u, v; cin >> u >> v;
+
+        U[i] = u;
+        V[i] = v;
+
+        g[v].push_back(u);
+    }
+
+    vector<int> DP(n + 1);
+    DP[1] = 0;
+
+    for (int i = 2; i <= n; i++) {
+        DP[i] = DP[i - 1] + 1;
+
+        for (auto u : g[i]) {
+            DP[i] = min(DP[i], DP[u] + 1);
+        }
+    }
+
+    vector<int> ans(n + 1, 0);
+    SegmentTree T(ans);
+
+    for (int i = 0; i < m; i++) {
+        int u = U[i];
+        int v = V[i];
+
+        if (u + 1 > v - 2 - DP[u]) {
+            continue;
+        }
+
+        T.plus(u + 1, v - 2 - DP[u], 1);
+    }
+
+    for (int i = 1; i < (int)ans.size() - 1; i++) {
+        if (T.query(i, i) == 0) {
+            cout << 1;
+        }
+        else {
+            cout << 0;
+        }
+    } cout << endl;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t; cin >> t;
+    for (int i = 0; i < t; i++) {
+        Solve();
+    }
+
+    return 0;
+}
