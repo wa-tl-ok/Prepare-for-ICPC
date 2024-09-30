fb5cdd86e217f2c99ef7f691d345921a7368ee02
Dmitriy <165094851+wa-tl-ok@users.noreply.github.com>
2024-09-30 17:38:42 +0300

    Create Task 9.cpp

---
+++ Tasks Fest/DP/Theme 6/Task 9.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <array>
+#include <list>
+#include <stack>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <string>
+#include <cstring>
+#include <random>
+#include <bitset>
+#include <functional>
+
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> M(n + 1);
+    for (int i = 1; i <= n; i++) {
+        cin >> M[i];
+    }
+
+    vector<int> C(n + 1);
+    for (int i = 1; i <= n; i++) {
+        cin >> C[i];
+    }
+
+    vector<vector<int>> DP(m + 1);
+    for (int i = 0; i < m + 1; i++) {
+        DP[i].resize(n + 1);
+    }
+
+    for (int i = 0; i < m + 1; i++) {
+        DP[i][0] = 0;
+    }
+
+    for (int i = 0; i < n + 1; i++) {
+        DP[0][i] = 0;
+    }
+
+    for (int i = 1; i <= m; i++) {
+        for (int j = 1; j <= n; j++) {
+            DP[i][j] = DP[i][j - 1];
+            if (i - M[j] >= 0) {
+                DP[i][j] = max(DP[i][j], DP[i - M[j]][j - 1] + C[j]);
+            }
+        }
+    }
+
+    int ans = 0;
+    for (int i = 0; i <= m; i++) {
+        for (int j = 1; j <= n; j++) {
+            ans = max(ans, DP[i][j]);
+        }
+    }
+
+    cout << ans;
+
+    return 0;
+}
