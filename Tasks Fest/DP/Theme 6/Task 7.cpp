a15b2101a241fa0b122240f8e033a5ad092171f0
Dmitriy <165094851+wa-tl-ok@users.noreply.github.com>
2024-10-02 19:46:45 +0300

    Update Task 7.cpp

--- Tasks Fest/DP/Theme 6/Task 7.cpp
+++ Tasks Fest/DP/Theme 6/Task 7.cpp
@@ -20,6 +20,10 @@
 using namespace std;
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
     int n; cin >> n;
     vector<int> s(n);
     for (int i = 0; i < n; i++) {
