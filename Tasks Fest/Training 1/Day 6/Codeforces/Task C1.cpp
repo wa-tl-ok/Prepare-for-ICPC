62ffd7886355fd8dd5a27330d0edfd611eb84082
Dmitriy <165094851+wa-tl-ok@users.noreply.github.com>
2024-10-06 22:59:40 +0300

    Create Task C1.cpp

---
+++ Tasks Fest/Training 1/Day 6/Codeforces/Task C1.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <array>
+#include <list>
+#include <stack>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <string>
+#include <cstring>
+#include <random>
+#include <bitset>
+#include <functional>
+
+using namespace std;
+
+#define int long long
+
+void Solve() { 
+    int n; cin >> n;
+    vector<int> a(n + 1);
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+    }
+    vector<int>dp(n + 1);
+    dp[0] = 0;
+    for (int i = 1; i <= n; i++) {
+        dp[i] = min(dp[i - 1] + 1, a[i]);
+    }
+    int S = 0;
+    for (int i = 0; i < dp.size(); i++) {
+        S += dp[i];
+    }
+    cout << S << '\n';
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int multitest = true;
+    if (multitest == true) {
+        int ctt; cin >> ctt;
+
+        for (int i = 0; i < ctt; i++) {
+            Solve();
+        }
+    }
+    else {
+        Solve();
+    }
+
+    return 0;
+}
