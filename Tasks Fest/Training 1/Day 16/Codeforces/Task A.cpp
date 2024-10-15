0bda6af88572285b70f233cc2f0e332a0c4ca467
Dmitriy <165094851+wa-tl-ok@users.noreply.github.com>
2024-10-15 16:39:23 +0300

    Create Task A.cpp

---
+++ Tasks Fest/Training 1/Day 16/Codeforces/Task A.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <array>
+#include <list>
+#include <stack>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <string>
+#include <cstring>
+#include <random>
+#include <bitset>
+#include <functional>
+
+using namespace std;
+
+void Solve() {
+    int n; cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    vector<int> L(105);
+    for (int i = 0; i < n; i++) {
+        L[a[i]] += 1;
+    }
+    for (int i = 0; i < 103; i++) {
+        if (L[i + 1] > L[i]) {
+            cout << "NO\n";
+            return;
+        }
+    }
+    cout << "YES\n";
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int multitest = true;
+    if (multitest == true) {
+        int ctt; cin >> ctt;
+
+        for (int i = 0; i < ctt; i++) {
+            Solve();
+        }
+    }
+    else {
+        Solve();
+    }
+
+    return 0;
+}
