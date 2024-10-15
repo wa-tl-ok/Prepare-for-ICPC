8e93547038e2a4e8445e69615ba98add7f9fc0d5
Dmitriy <165094851+wa-tl-ok@users.noreply.github.com>
2024-10-16 01:27:36 +0300

    Create Task A.cpp

---
+++ Tasks Fest/Training 1/Day 17/Atcoder/Task A.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <array>
+#include <list>
+#include <stack>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <string>
+#include <cstring>
+#include <random>
+#include <bitset>
+#include <functional>
+
+using namespace std;
+
+#define int long long
+
+void Solve() {
+    int n, x; cin >> n >> x;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++) {
+        if (a[i] == x) {
+            cout << i + 1;
+            return;
+        }
+    }
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int multitest = false;
+    if (multitest == true) {
+        int ctt; cin >> ctt;
+
+        for (int i = 0; i < ctt; i++) {
+            Solve();
+        }
+    }
+    else {
+        Solve();
+    }
+
+    return 0;
+}
