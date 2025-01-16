b2a0778b4090fb2ae7f6e8516cf8237041c81da7
Dmitriy <165094851+wa-tl-ok@users.noreply.github.com>
2025-01-17 02:21:16 +0300

    Create Suffix Automaton.cpp

---
+++ Theory/String algorithms/Suffix Automaton/Suffix Automaton.cpp
@@ -0,0 +1,78 @@
+class SuffixAutomaton {
+public:
+    struct Node {
+        int len;
+        Node* link;
+        map<char, Node*> next;
+
+        Node(int length) : len(length), link(nullptr) {}
+    };
+
+    SuffixAutomaton() {
+        root = new Node(0);
+        last = root;
+        states.push_back(root);
+    }
+
+    void go(char c) {
+        Node* cur = new Node(last->len + 1);
+        states.push_back(cur);
+
+        Node* p = last;
+        while (p && !p->next.count(c)) {
+            p->next[c] = cur;
+            p = p->link;
+        }
+
+        if (!p) {
+            cur->link = root;
+        }
+        else {
+            Node* q = p->next[c];
+
+            if (p->len + 1 == q->len) {
+                cur->link = q;
+            }
+            else {
+                Node* clone = new Node(p->len + 1);
+                states.push_back(clone);
+
+                clone->next = q->next;
+                clone->link = q->link;
+
+                while (p && p->next[c] == q) {
+                    p->next[c] = clone;
+                    p = p->link;
+                }
+
+                q->link = clone;
+                cur->link = clone;
+            }
+        }
+
+        last = cur;
+    }
+
+    /*
+        Solving problem:
+            Given a string S [alh = {'a', 'b' ... 'z'}].
+            Find the number of different substrings of the string S.
+
+            --- O(S.size() + SuffixAutomaton.size()) ---
+    */
+
+    int Solve() {
+        int result = 0;
+        for (Node* state : states) {
+            if (state != root) {
+                result += state->len - state->link->len;
+            }
+        }
+        return result;
+    }
+
+private:
+    Node* root;
+    Node* last;
+    vector<Node*> states;
+};
