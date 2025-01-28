0cc81fcc0dde5ed9229f64cf4d7e4a9875b2a3a3
Dmitriy <165094851+wa-tl-ok@users.noreply.github.com>
2025-01-28 07:59:22 +0300

    Update Matrix.cpp

--- Theory/Math/Matrix/Matrix.cpp
+++ Theory/Math/Matrix/Matrix.cpp
@@ -6,17 +6,7 @@ public:
         return data;
     }
 
-    Matrix transpose() const {
-        vector<vector<int>> transposed_matrix(data[0].size(), vector<int>(data.size(), 0));
-        for (size_t i = 0; i < data.size(); ++i) {
-            for (size_t j = 0; j < data[0].size(); ++j) {
-                transposed_matrix[j][i] = data[i][j];
-            }
-        }
-        return Matrix(transposed_matrix);
-    }
-
-    Matrix rotateClockwise() const {
+    Matrix rotateclockwise() const {
         vector<vector<int>> rotated_matrix(data[0].size(), vector<int>(data.size(), 0));
         for (size_t i = 0; i < data.size(); ++i) {
             for (size_t j = 0; j < data[0].size(); ++j) {
@@ -78,10 +68,6 @@ public:
         return Matrix(result_matrix);
     }
 
-    Matrix operator/(const Matrix& other) const {
-        return *this * other.inverse();
-    }
-
     bool operator==(const Matrix& other) const {
         return data == other.data;
     }
@@ -109,29 +95,6 @@ public:
         return result;
     }
 
-    int determinant() const {
-        size_t n = data.size();
-        if (n == 0) {
-            throw invalid_argument("Determinant is not defined for an empty matrix.");
-        }
-        if (n != data[0].size()) {
-            throw invalid_argument("Determinant is defined only for square matrices.");
-        }
-
-        if (n == 1) {
-            return data[0][0];
-        }
-        if (n == 2) {
-            return data[0][0] * data[1][1] - data[0][1] * data[1][0];
-        }
-
-        int det = 0;
-        for (size_t j = 0; j < n; ++j) {
-            det += (j % 2 == 0 ? 1 : -1) * data[0][j] * minor(0, j).determinant();
-        }
-        return det;
-    }
-
     void print() const {
         for (const auto& row : data) {
             for (const auto& elem : row) {
@@ -150,70 +113,5 @@ private:
             identity_matrix[i][i] = 1;
         }
         return Matrix(identity_matrix);
-    }
-
-    static Matrix identity(size_t size) {
-        vector<vector<int>> identity_matrix(size, vector<int>(size, 0));
-        for (size_t i = 0; i < size; ++i) {
-            identity_matrix[i][i] = 1;
-        }
-        return Matrix(identity_matrix);
-    }
-
-    Matrix inverse() const {
-        size_t n = data.size();
-        vector<vector<int>> augmented_matrix(n, vector<int>(2 * n, 0));
-
-        for (size_t i = 0; i < n; ++i) {
-            for (size_t j = 0; j < n; ++j) {
-                augmented_matrix[i][j] = data[i][j];
-            }
-            augmented_matrix[i][i + n] = 1;
-        }
-
-        for (size_t i = 0; i < n; ++i) {
-            if (augmented_matrix[i][i] == 0) {
-                throw invalid_argument("Matrix is singular and cannot be inverted.");
-            }
-
-            int pivot = augmented_matrix[i][i];
-            for (size_t j = 0; j < 2 * n; ++j) {
-                augmented_matrix[i][j] /= pivot;
-            }
-
-            for (size_t j = 0; j < n; ++j) {
-                if (j != i) {
-                    int factor = augmented_matrix[j][i];
-                    for (size_t k = 0; k < 2 * n; ++k) {
-                        augmented_matrix[j][k] -= factor * augmented_matrix[i][k];
-                    }
-                }
-            }
-        }
-
-        vector<vector<int>> inverse_matrix(n, vector<int>(n, 0));
-        for (size_t i = 0; i < n; ++i) {
-            for (size_t j = 0; j < n; ++j) {
-                inverse_matrix[i][j] = augmented_matrix[i][j + n];
-            }
-        }
-
-        return Matrix(inverse_matrix);
-    }
-
-    Matrix minor(size_t row, size_t col) const {
-        vector<vector<int>> minor_matrix;
-        for (size_t i = 0; i < data.size(); ++i) {
-            if (i != row) {
-                vector<int> new_row;
-                for (size_t j = 0; j < data[i].size(); ++j) {
-                    if (j != col) {
-                        new_row.push_back(data[i][j]);
-                    }
-                }
-                minor_matrix.push_back(new_row);
-            }
-        }
-        return Matrix(minor_matrix);
     }
 };
