5a0fd843b8e4f4ffeb2bb72f4808afeca5963759
Dmitriy <165094851+wa-tl-ok@users.noreply.github.com>
2025-02-27 21:22:13 +0300

    Create Floyd–Warshall algorithm.cpp

---
+++ Theory/Graphs/Shortest paths in graphs/Find the negative weight cycle/Floyd–Warshall algorithm.cpp
@@ -0,0 +1,196 @@
+// #define _CRT_SECURE_NO_WARNINGS 
+
+#include <iostream> 
+#include <algorithm> 
+#include <cmath> 
+#include <climits> 
+#include <vector> 
+#include <queue> 
+#include <deque> 
+#include <array> 
+#include <list> 
+#include <stack> 
+#include <tuple> 
+#include <set> 
+#include <unordered_set> 
+#include <map> 
+#include <unordered_map> 
+#include <string> 
+#include <cstring> 
+#include <random> 
+#include <bitset> 
+#include <iomanip> 
+#include <iterator> 
+#include <functional> 
+#include <ctime> 
+#include <chrono> 
+#include <cctype> 
+#include <fstream> 
+#include <ranges> 
+#include <numeric> 
+#include <complex> 
+#include <cassert> 
+
+using namespace std;
+
+// #pragma GCC optimize("Ofast") 
+// #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+#define int               long long 
+#define sz(x)             ((int)(x).size()) 
+#define mp                make_pair 
+#define all(x)            (x).begin(), (x).end() 
+#define pb                push_back 
+#define pf                push_front 
+#define ff                first 
+#define ss                second 
+#define unique(x)         (x).erase(unique(all(x)), (x).end()) 
+#define min3(a, b, c)     min(a, min(b, c)) 
+#define max3(a, b, c)     max(a, max(b, c)) 
+#define FOR(i, a, b)      for (int i = (a); i <= (b); i++) 
+#define ROF(i, a, b)      for (int i = (a); i >= (b); i--) 
+
+using vi = vector<int>;
+using vd = vector<double>;
+using vpii = vector<pair<int, int>>;
+using vpdd = vector<pair<double, double>>;
+using pii = pair<int, int>;
+using pdd = pair<double, double>;
+
+template <typename Container>
+void PRINT(const Container& container) {
+    for (const auto& e : container) {
+        cout << e << ' ';
+    } cout << '\n';
+}
+
+void print_double(double ans, int num) {
+    cout << fixed << setprecision(num) << ans << '\n';
+}
+
+const int inf = 1e18;
+const double eps = 1e-9;
+const double PI = 3.141592653589793;
+
+string alh = "abcdefghijklmnopqrstuvwxyz";
+string ALH = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+void Solve() {
+    const int no_edge = 123456789;
+    int n; cin >> n;
+
+    vector<vector<long long>> M(n);
+    for (int i = 0; i < n; i++) {
+        M[i].resize(n);
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> M[i][j];
+        }
+    }
+
+    vector<vector<long long>> Next(n);
+    for (int i = 0; i < n; i++) {
+        Next[i].resize(n);
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (M[i][j] != no_edge) {
+                Next[i][j] = j;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int u = 0; u < n; u++) {
+            for (int v = 0; v < n; v++) {
+                if (M[u][i] != no_edge && M[i][v] != no_edge && M[u][v] > M[u][i] + M[i][v]) {
+                    M[u][v] = M[u][i] + M[i][v];
+                    Next[u][v] = Next[u][i];
+
+                    if (M[u][v] < 0 && u == v) {
+                        cout << "YES" << "\n";
+
+                        vector<int> ans;
+
+                        int now = u;
+                        int finish = u;
+
+                        ans.push_back(now);
+                        now = Next[now][finish];
+
+                        while (now != finish) {
+                            ans.push_back(now);
+                            now = Next[now][finish];
+                        }
+
+                        ans.push_back(now);
+                        now = Next[now][finish];
+
+                        cout << ans.size() << "\n";
+                        for (auto q : ans) {
+                            cout << q + 1 << ' ';
+                        }
+
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    cout << "NO";
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    /*
+                      ________________
+                     / Just solved it \
+                     |   in my mind   |
+                     \________________/
+                    /
+                   /
+　　　　　／＞　 フ         ___________
+　　　　　| 　_　 _|       |          |
+　 　　　／`ミ _x 彡       |   WA 2   |
+　　 　 /　　　 　 |       |__________|
+　　　 /　 ヽ　　 ﾉ        /          /
+　／￣|　　 |　|　|       /_________ /
+　| (￣ヽ＿_ヽ_)_)
+　＼二つ
+
+    */
+
+    /*
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    */
+
+    // auto start = chrono::high_resolution_clock::now();
+
+    int multitest = false;
+    if (multitest == true) {
+        int ctt; cin >> ctt;
+
+        for (int i = 0; i < ctt; i++) {
+            Solve();
+        }
+    }
+    else {
+        Solve();
+    }
+
+    // auto end = chrono::high_resolution_clock::now();
+
+    /*
+    cout << "Time taken: "
+         << chrono::duration_cast<chrono::milliseconds>(end - start).count()
+         << " milliseconds" << endl;
+    */
+
+    return 0;
+}
