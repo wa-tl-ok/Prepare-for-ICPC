cb385d5bf7d5cc95be6b6287f0ab619ebfa50dcb
Dmitriy <165094851+wa-tl-ok@users.noreply.github.com>
2025-02-27 17:58:03 +0300

    Create Task 1.cpp

---
+++ Tasks/Graphs/HLD/Task 1.cpp
@@ -0,0 +1,37 @@
+//https://atcoder.jp/contests/abc368/tasks/abc368_d
+
+void Solve() {
+    int n; cin >> n;
+    int k; cin >> k;
+
+    HLD hld(n);
+
+    for (int i = 0; i < n - 1; i++) {
+        int u, v; cin >> u >> v;
+        --u;
+        --v;
+        hld.Add_edge(u, v);
+    }
+
+    hld.Decompose();
+
+    vector<int> Upd(k);
+    for (int i = 0; i < k; i++) {
+        cin >> Upd[i];
+        Upd[i] -= 1;
+    }
+
+    hld.change(Upd[0], 1);
+    for (int i = 1; i < k; i++) {
+        hld.change(Upd[0], Upd[i], 1);
+    }
+
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        if (hld.Get_sum(i, i) > 0) {
+            ++ans;
+        }
+    }
+
+    cout << ans;
+}
