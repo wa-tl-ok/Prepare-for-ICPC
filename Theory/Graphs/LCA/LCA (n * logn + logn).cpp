4a6c006cb3687aa2ad6a2b7e356b33619c6f38e7
Dmitriy <165094851+wa-tl-ok@users.noreply.github.com>
2025-02-27 20:30:20 +0300

    Update LCA (n * logn + logn).cpp

--- Theory/Graphs/LCA/LCA (n * logn + logn).cpp
+++ Theory/Graphs/LCA/LCA (n * logn + logn).cpp
@@ -1,13 +1,16 @@
-#include <iostream>
-#include <iomanip> 
+// #define _CRT_SECURE_NO_WARNINGS 
+
+#include <iostream> 
 #include <algorithm> 
 #include <cmath> 
+#include <climits> 
 #include <vector> 
 #include <queue> 
 #include <deque> 
 #include <array> 
 #include <list> 
 #include <stack> 
+#include <tuple> 
 #include <set> 
 #include <unordered_set> 
 #include <map> 
@@ -16,13 +19,61 @@
 #include <cstring> 
 #include <random> 
 #include <bitset> 
+#include <iomanip> 
+#include <iterator> 
 #include <functional> 
-#include <cstdlib>
-#include <ctime>
-#include <climits>
-#include <cassert>
+#include <ctime> 
+#include <chrono> 
+#include <cctype> 
+#include <fstream> 
+#include <ranges> 
+#include <numeric> 
+#include <complex> 
+#include <cassert> 
 
 using namespace std;
+
+// #pragma GCC optimize("Ofast") 
+// #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+#define int               long long 
+#define sz(x)             ((int)(x).size()) 
+#define mp                make_pair 
+#define all(x)            (x).begin(), (x).end() 
+#define pb                push_back 
+#define pf                push_front 
+#define ff                first 
+#define ss                second 
+#define unique(x)         (x).erase(unique(all(x)), (x).end()) 
+#define min3(a, b, c)     min(a, min(b, c)) 
+#define max3(a, b, c)     max(a, max(b, c)) 
+#define FOR(i, a, b)      for (int i = (a); i <= (b); i++) 
+#define ROF(i, a, b)      for (int i = (a); i >= (b); i--) 
+
+using vi = vector<int>;
+using vd = vector<double>;
+using vpii = vector<pair<int, int>>;
+using vpdd = vector<pair<double, double>>;
+using pii = pair<int, int>;
+using pdd = pair<double, double>;
+
+template <typename Container>
+void PRINT(const Container& container) {
+    for (const auto& e : container) {
+        cout << e << ' ';
+    } cout << '\n';
+}
+
+void print_double(double ans, int num) {
+    cout << fixed << setprecision(num) << ans << '\n';
+}
+
+const int inf = 1e18;
+const double eps = 1e-9;
+const double PI = 3.141592653589793;
+
+string alh = "abcdefghijklmnopqrstuvwxyz";
+string ALH = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 const int LOGN = 23;
 
@@ -70,14 +121,7 @@ int lca(int u, int v) {
     return parent[u][0];
 }
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-
-    freopen("lca.in", "r", stdin);
-    freopen("lca.out", "w", stdout);
-
+void Solve() {
     int n; cin >> n;
 
     graph.resize(n);
@@ -107,6 +151,57 @@ int main() {
     for (const auto& query : queries) {
         cout << lca(query.first, query.second) + 1 << "\n";
     }
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    /*
+                      ________________
+                     / Just solved it \
+                     |   in my mind   |
+                     \________________/
+                    /
+                   /
+　　　　　／＞　 フ         ___________
+　　　　　| 　_　 _|       |          |
+　 　　　／`ミ _x 彡       |   WA 2   |
+　　 　 /　　　 　 |       |__________|
+　　　 /　 ヽ　　 ﾉ        /          /
+　／￣|　　 |　|　|       /_________ /
+　| (￣ヽ＿_ヽ_)_)
+　＼二つ
+
+    */
+
+    /*
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    */
+
+    // auto start = chrono::high_resolution_clock::now();
+
+    int multitest = false;
+    if (multitest == true) {
+        int ctt; cin >> ctt;
+
+        for (int i = 0; i < ctt; i++) {
+            Solve();
+        }
+    }
+    else {
+        Solve();
+    }
+
+    // auto end = chrono::high_resolution_clock::now();
+
+    /*
+    cout << "Time taken: "
+         << chrono::duration_cast<chrono::milliseconds>(end - start).count()
+         << " milliseconds" << endl;
+    */
 
     return 0;
 }
