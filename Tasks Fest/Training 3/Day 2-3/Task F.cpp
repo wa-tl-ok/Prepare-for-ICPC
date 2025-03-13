0c900123e0f725c5e7f74e3fab32673294193a8e
Dmitriy <165094851+wa-tl-ok@users.noreply.github.com>
2025-03-13 22:05:30 +0300

    Create Task F.cpp

---
+++ Tasks Fest/Training 3/Day 2-3/Task F.cpp
@@ -0,0 +1,243 @@
+// #define _CRT_SECURE_NO_WARNINGS 
+
+#include <iostream> 
+#include <algorithm> 
+#include <cmath> 
+#include <climits> 
+#include <vector> 
+#include <queue> 
+#include <deque> 
+#include <array> 
+#include <list> 
+#include <stack> 
+#include <tuple> 
+#include <set> 
+#include <unordered_set> 
+#include <map> 
+#include <unordered_map> 
+#include <string> 
+#include <cstring> 
+#include <random> 
+#include <bitset> 
+#include <iomanip> 
+#include <iterator> 
+#include <functional> 
+#include <ctime> 
+#include <chrono> 
+#include <cctype> 
+#include <fstream> 
+#include <ranges> 
+#include <numeric> 
+#include <complex> 
+#include <cassert> 
+
+using namespace std;
+
+// #pragma GCC optimize("Ofast") 
+// #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+#define int               long long 
+#define sz(x)             ((int)(x).size()) 
+#define mp                make_pair 
+#define all(x)            (x).begin(), (x).end() 
+#define pb                push_back 
+#define pf                push_front 
+#define ff                first 
+#define ss                second 
+#define unique(x)         (x).erase(unique(all(x)), (x).end()) 
+#define min3(a, b, c)     min(a, min(b, c)) 
+#define max3(a, b, c)     max(a, max(b, c)) 
+#define FOR(i, a, b)      for (int i = (a); i <= (b); i++) 
+#define ROF(i, a, b)      for (int i = (a); i >= (b); i--) 
+
+using vi = vector<int>;
+using vd = vector<double>;
+using vpii = vector<pair<int, int>>;
+using vpdd = vector<pair<double, double>>;
+using pii = pair<int, int>;
+using pdd = pair<double, double>;
+
+template <typename Container>
+void PRINT(const Container& container) {
+    for (const auto& e : container) {
+        cout << e << ' ';
+    } cout << '\n';
+}
+
+void print_double(double ans, int num) {
+    cout << fixed << setprecision(num) << ans << '\n';
+}
+
+const int inf = 1e18;
+const double eps = 1e-9;
+const double PI = 3.141592653589793;
+
+string alh = "abcdefghijklmnopqrstuvwxyz";
+string ALH = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+class SegmentTree {
+public:
+    SegmentTree(int size) : n(size), root(nullptr) {}
+
+    void change(int pos, int val) {
+        root = updatePoint(root, 0, n - 1, pos, val);
+    }
+
+    int query(int left, int right) {
+        return queryRange(root, 0, n - 1, left, right);
+    }
+
+private:
+    struct Node {
+        int maxVal;
+        Node* left;
+        Node* right;
+
+        Node() : maxVal(std::numeric_limits<int>::min()), left(nullptr), right(nullptr) {}
+    };
+
+    Node* root;
+    int n;
+
+    Node* updatePoint(Node* node, int l, int r, int pos, int val) {
+        if (!node) {
+            node = new Node();
+        }
+
+        if (l == r) {
+            node->maxVal = val;
+            return node;
+        }
+
+        int mid = l + (r - l) / 2;
+
+        if (pos <= mid) {
+            node->left = updatePoint(node->left, l, mid, pos, val);
+        }
+        else {
+            node->right = updatePoint(node->right, mid + 1, r, pos, val);
+        }
+
+        node->maxVal = max(getMax(node->left), getMax(node->right));
+
+        return node;
+    }
+
+    int queryRange(Node* node, int l, int r, int const_l, int const_r) {
+        if (!node) {
+            return std::numeric_limits<int>::min();
+        }
+
+        if (const_l > r || const_r < l) {
+            return std::numeric_limits<int>::min();
+        }
+
+        if (const_l <= l && r <= const_r) {
+            return node->maxVal;
+        }
+
+        int mid = l + (r - l) / 2;
+
+        return max(queryRange(node->left, l, mid, const_l, const_r),
+            queryRange(node->right, mid + 1, r, const_l, const_r));
+    }
+
+    int getMax(Node* node) {
+        return node ? node->maxVal : std::numeric_limits<int>::min();
+    }
+};
+
+void Solve() {
+    int n; cin >> n;
+    vector<int> a(n + 1);
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+    }
+
+    // 'coordinate' compression start
+
+    vector<int> pref_sums = { 0 };
+    int S = 0;
+    for (int i = 1; i <= n; i++) {
+        S += a[i];
+        pref_sums.push_back(S);
+    }
+    set<int> dop;
+    for (int i = 0; i <= n; i++) {
+        dop.insert(pref_sums[i] - 1);
+        dop.insert(pref_sums[i]);
+        dop.insert(pref_sums[i] + 1);
+    }
+    map<int, int> ID;
+    int cur = 0;
+    for (auto p : dop) {
+        ID[p] = cur;
+        ++cur;
+    }
+
+    // 'coordinate' compression end
+    
+    SegmentTree ST(4 * n);
+    ST.change(ID[0], 0);
+
+    vector<int> dp(n + 1);
+    for (int i = 1; i <= n; i++) {
+        dp[i] = max(dp[i - 1] + (a[i] < 0 ? -1 : a[i] > 0 ? 1 : 0), ST.query(0, ID[pref_sums[i] - 1]) + i);
+        ST.change(ID[pref_sums[i]], max(dp[i] - i, ST.query(ID[pref_sums[i]], ID[pref_sums[i]])));
+    }
+
+    cout << dp[n] << '\n';
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    /*
+                      ________________
+                     / Just solved it \
+                     |   in my mind   |
+                     \________________/
+                    /
+                   /
+　　　　　／＞　 フ         ___________
+　　　　　| 　_　 _|       |          |
+　 　　　／`ミ _x 彡       |   WA 2   |
+　　 　 /　　　 　 |       |__________|
+　　　 /　 ヽ　　 ﾉ        /          /
+　／￣|　　 |　|　|       /_________ /
+　| (￣ヽ＿_ヽ_)_)
+　＼二つ
+
+    */
+
+    /*
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    */
+
+    // auto start = chrono::high_resolution_clock::now();
+
+    int multitest = true;
+    if (multitest == true) {
+        int ctt; cin >> ctt;
+
+        for (int i = 0; i < ctt; i++) {
+            Solve();
+        }
+    }
+    else {
+        Solve();
+    }
+
+    // auto end = chrono::high_resolution_clock::now();
+
+    /*
+    cout << "Time taken: "
+         << chrono::duration_cast<chrono::milliseconds>(end - start).count()
+         << " milliseconds" << endl;
+    */
+
+    return 0;
+}
