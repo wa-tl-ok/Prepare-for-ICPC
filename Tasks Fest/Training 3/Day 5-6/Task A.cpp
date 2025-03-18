0d037cf07beb0cc575cd7dba13222295bef68cfd
Dmitriy <165094851+wa-tl-ok@users.noreply.github.com>
2025-03-19 02:13:44 +0300

    Create Task A.cpp

---
+++ Tasks Fest/Training 3/Day 5-6/Task A.cpp
@@ -0,0 +1,186 @@
+// #define _CRT_SECURE_NO_WARNINGS 
+
+#include <iostream> 
+#include <algorithm> 
+#include <cmath> 
+#include <climits> 
+#include <vector> 
+#include <queue> 
+#include <deque> 
+#include <array> 
+#include <list> 
+#include <stack> 
+#include <tuple> 
+#include <set> 
+#include <unordered_set> 
+#include <map> 
+#include <unordered_map> 
+#include <string> 
+#include <cstring> 
+#include <random> 
+#include <bitset> 
+#include <iomanip> 
+#include <iterator> 
+#include <functional> 
+#include <ctime> 
+#include <chrono> 
+#include <cctype> 
+#include <fstream> 
+#include <ranges> 
+#include <numeric> 
+#include <complex> 
+#include <cassert> 
+
+using namespace std;
+
+// #pragma GCC optimize("Ofast") 
+// #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+#define int               long long 
+#define sz(x)             ((int)(x).size()) 
+#define mp                make_pair 
+#define all(x)            (x).begin(), (x).end() 
+#define pb                push_back 
+#define pf                push_front 
+#define ff                first 
+#define ss                second 
+#define unique(x)         (x).erase(unique(all(x)), (x).end()) 
+#define min3(a, b, c)     min(a, min(b, c)) 
+#define max3(a, b, c)     max(a, max(b, c)) 
+#define FOR(i, a, b)      for (int i = (a); i <= (b); i++) 
+#define ROF(i, a, b)      for (int i = (a); i >= (b); i--) 
+
+using vi = vector<int>;
+using vd = vector<double>;
+using vpii = vector<pair<int, int>>;
+using vpdd = vector<pair<double, double>>;
+using pii = pair<int, int>;
+using pdd = pair<double, double>;
+
+template <typename Container>
+void PRINT(const Container& container) {
+    for (const auto& e : container) {
+        cout << e << ' ';
+    } cout << '\n';
+}
+
+void print_double(double ans, int num) {
+    cout << fixed << setprecision(num) << ans << '\n';
+}
+
+const int inf = 1e18;
+const double eps = 1e-9;
+const double PI = 3.141592653589793;
+
+string alh = "abcdefghijklmnopqrstuvwxyz";
+string ALH = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+void Solve() {
+    int n, m; cin >> n >> m;
+    string S; cin >> S;
+    
+    int L = 0;
+    int R = 0;
+
+    int U = 0;
+    int D = 0;
+
+    int cur_x = 0;
+    int cur_y = 0;
+    for (int i = 0; i < S.size(); i++) {
+        int WL = L;
+        int WU = U;
+
+        if (S[i] == 'L') {
+            --cur_x;
+        }
+        else if (S[i] == 'R') {
+            ++cur_x;
+        }
+        else if (S[i] == 'U') {
+            --cur_y;
+        }
+        else {
+            ++cur_y;
+        }
+
+        if (cur_x < 0) {
+            L = min(L, cur_x);
+        }
+        else {
+            R = max(R, cur_x);
+        }
+
+        if (cur_y < 0) {
+            U = min(U, cur_y);
+        }
+        else {
+            D = max(D, cur_y);
+        }
+
+        if (R - L + 1 > m) {
+            cout << abs(WU) + 1 << ' ' << abs(WL) + 1 << '\n';
+            return;
+        }
+        
+        if (D - U + 1 > n) {
+            cout << abs(WU) + 1 << ' ' << abs(WL) + 1 << '\n';
+            return;
+        }
+    }
+
+    cout << abs(U) + 1 << ' ' << abs(L) + 1 << '\n';
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    /*
+                      ________________
+                     / Just solved it \
+                     |   in my mind   |
+                     \________________/
+                    /
+                   /
+　　　　　／＞　 フ         ___________
+　　　　　| 　_　 _|       |          |
+　 　　　／`ミ _x 彡       |   WA 2   |
+　　 　 /　　　 　 |       |__________|
+　　　 /　 ヽ　　 ﾉ        /          /
+　／￣|　　 |　|　|       /_________ /
+　| (￣ヽ＿_ヽ_)_)
+　＼二つ
+
+    */
+
+    /*
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    */
+
+    // auto start = chrono::high_resolution_clock::now();
+
+    int multitest = true;
+    if (multitest == true) {
+        int ctt; cin >> ctt;
+
+        for (int i = 0; i < ctt; i++) {
+            Solve();
+        }
+    }
+    else {
+        Solve();
+    }
+
+    // auto end = chrono::high_resolution_clock::now();
+
+    /*
+    cout << "Time taken: "
+         << chrono::duration_cast<chrono::milliseconds>(end - start).count()
+         << " milliseconds" << endl;
+    */
+
+    return 0;
+}
