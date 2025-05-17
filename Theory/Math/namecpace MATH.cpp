0208449e2bd96861f8697f9d80d5856c170e966a
Dmitriy <165094851+wa-tl-ok@users.noreply.github.com>
2025-05-17 23:06:36 +0300

    Update namecpace MATH.cpp

--- Theory/Math/namecpace MATH.cpp
+++ Theory/Math/namecpace MATH.cpp
@@ -227,4 +227,28 @@ namespace MATH {
         }
         return left;
     }
+
+    vector<int> sieve_of_eratosthenes(int n) {
+        if (n < 2) return {};
+
+        vector<bool> is_prime(n + 1, true);
+        is_prime[0] = is_prime[1] = false;
+
+        for (int p = 2; p * p <= n; ++p) {
+            if (is_prime[p]) {
+                for (int i = p * p; i <= n; i += p) {
+                    is_prime[i] = false;
+                }
+            }
+        }
+
+        vector<int> primes;
+        for (int i = 2; i <= n; ++i) {
+            if (is_prime[i]) {
+                primes.push_back(i);
+            }
+        }
+
+        return primes;
+    }
 }
