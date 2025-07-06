a7b21ac2f9c1c62ed6ad1a58d7c409e325567cf1
Dmitriy <165094851+rewatlok@users.noreply.github.com>
2025-07-06 03:28:08 +0300

    Create print-the-route-of-the-maximum-cost.cpp

---
+++ coderun/print-the-route-of-the-maximum-cost.cpp
@@ -0,0 +1,230 @@
+// https://coderun.yandex.ru/problem/print-the-route-of-the-maximum-cost
+
+// #define _CRT_SECURE_NO_WARNINGS 
+
+#include <iostream> 
+#include <algorithm> 
+#include <cmath> 
+#include <climits> 
+#include <vector> 
+#include <queue> 
+#include <deque> 
+#include <array> 
+#include <list> 
+#include <stack> 
+#include <tuple> 
+#include <set> 
+#include <unordered_set> 
+#include <map> 
+#include <unordered_map> 
+#include <string> 
+#include <cstring> 
+#include <random> 
+#include <bitset> 
+#include <iomanip> 
+#include <iterator> 
+#include <functional> 
+#include <ctime> 
+#include <chrono> 
+#include <cctype> 
+#include <fstream> 
+#include <numeric> 
+#include <complex> 
+#include <cassert> 
+
+using namespace std;
+
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+#define int               long long 
+#define sz(x)             ((int)(x).size()) 
+#define mp                make_pair 
+#define all(x)            (x).begin(), (x).end() 
+#define pb                push_back 
+#define pf                push_front 
+#define ff                first 
+#define ss                second 
+#define unique(x)         (x).erase(unique(all(x)), (x).end()) 
+#define min3(a, b, c)     min(a, min(b, c)) 
+#define max3(a, b, c)     max(a, max(b, c)) 
+#define FOR(i, a, b)      for (int i = (a); i <= (b); i++) 
+#define ROF(i, a, b)      for (int i = (a); i >= (b); i--) 
+
+using vi = vector<int>;
+using vd = vector<double>;
+using vpii = vector<pair<int, int>>;
+using vpdd = vector<pair<double, double>>;
+using pii = pair<int, int>;
+using pdd = pair<double, double>;
+
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+// typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+template<typename T> using min_heap = priority_queue<T, vector<T>, greater<T>>;
+template<typename T> using max_heap = priority_queue<T>;
+
+#define BIT(n) (1LL << (n))
+#define HAS_BIT(x, n) (((x) >> (n)) & 1)
+#define SET_BIT(x, n) ((x) | BIT(n))
+
+template <typename Container>
+void PRINT(const Container& container) {
+    for (const auto& e : container) {
+        cout << e << ' ';
+    } cout << '\n';
+}
+
+void print_double(double ans, int num) {
+    cout << fixed << setprecision(num) << ans << '\n';
+}
+
+const int inf = 1e9;
+const double eps = 1e-9;
+const double PI = 3.141592653589793;
+
+string alh = "abcdefghijklmnopqrstuvwxyz";
+string ALH = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+void Solve() {
+    int n, m; cin >> n >> m;
+    vector<vector<int>> A(n);
+    for (int i = 0; i < n; i++) {
+        A[i].resize(m);
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> A[i][j];
+        }
+    }
+
+    vector<vector<int>> DP(n);
+    for (int i = 0; i < n; i++) {
+        DP[i].resize(m);
+    }
+
+    vector<vector<char>> ANS(n);
+    for (int i = 0; i < n; i++) {
+        ANS[i].resize(m);
+    }
+
+    DP[0][0] = A[0][0];
+    ANS[0][0] = 'S';
+    for (int i = 1; i < m; i++) {
+        DP[0][i] = DP[0][i - 1] + A[0][i];
+        ANS[0][i] = 'L';
+    }
+    for (int i = 1; i < n; i++) {
+        DP[i][0] = DP[i - 1][0] + A[i][0];
+        ANS[i][0] = 'U';
+    }
+
+    for (int i = 1; i < n; i++) {
+        for (int j = 1; j < m; j++) {
+            DP[i][j] = max(DP[i - 1][j], DP[i][j - 1]) + A[i][j];
+
+            if (DP[i - 1][j] < DP[i][j - 1]) {
+                ANS[i][j] = 'L';
+            }
+            else {
+                ANS[i][j] = 'U';
+            }
+        }
+    }
+
+    cout << DP[n - 1][m - 1] << '\n';
+
+    string ans = "";
+    int i = n - 1;
+    int j = m - 1;
+
+    while (i != 0 || j != 0) {
+        ans += ANS[i][j];
+
+        if (i == 0) {
+            --j;
+        }
+        else if (j == 0) {
+            --i;
+        }
+        else {
+            if (ANS[i][j] == 'L') {
+                --j;
+            }
+            else {
+                --i;
+            }
+        }
+    }
+
+    reverse(all(ans));
+
+    for (int i = 0; i < ans.size(); i++) {
+        if (ans[i] == 'L') {
+            ans[i] = 'R';
+        }
+        if (ans[i] == 'U') {
+            ans[i] = 'D';
+        }
+    }
+
+    for (int i = 0; i < ans.size(); i++) {
+        cout << ans[i] << ' ';
+    }
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    /*
+                      ________________
+                     / Just solved it \
+                     |   in my mind   |
+                     \________________/
+                    /
+                   /
+　　　　　／＞　 フ         ___________
+　　　　　| 　_　 _|       |          |
+　 　　　／`ミ _x 彡       |   WA 2   |
+　　 　 /　　　 　 |       |__________|
+　　　 /　 ヽ　　 ﾉ        /          /
+　／￣|　　 |　|　|       /_________ /
+　| (￣ヽ＿_ヽ_)_)
+　＼二つ
+
+    */
+
+    /*
+    freopen("task.in", "r", stdin);
+    freopen("task.out", "w", stdout);
+    */
+
+    // auto start = chrono::high_resolution_clock::now();
+
+    int multitest = false;
+    if (multitest == true) {
+        int ctt; cin >> ctt;
+
+        for (int i = 0; i < ctt; i++) {
+            Solve();
+        }
+    }
+    else {
+        Solve();
+    }
+
+    // auto end = chrono::high_resolution_clock::now();
+
+    /*
+    cout << "Time taken: "
+         << chrono::duration_cast<chrono::milliseconds>(end - start).count()
+         << " milliseconds" << endl;
+    */
+
+    return 0;
+}
